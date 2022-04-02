c7179e8249108df817759f2e31afafee3c6d5cb8
Kevin Jin <kevjin2002@gmail.com>
2022-04-02 03:49:27 -0500

    Added workshop3

--- todolist/src/components/Item.js
+++ todolist/src/components/Item.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Item({ itemData, removeItem }) {
   return (
     <div className="item">
-      <p>{itemData.title}</p>
+      <p className="title">{itemData.title}</p>
+      <p className="date">{itemData.date.toDateString()}</p>
       <input type="checkbox" onChange={() => removeItem(itemData)}></input>
     </div>
   );
--- todolist/src/components/List.js
+++ todolist/src/components/List.js
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import { v4 as uuidv4 } from "uuid";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function List() {
   let [title, setTitle] = useState("");
   let taskList = [
     {
       title: "task1",
+      date: new Date(),
+      id: uuidv4(),
     },
     {
       title: "task2",
+      date: new Date(),
+      id: uuidv4(),
     },
   ];
   let [todo, setTodo] = useState(taskList);
+  let [date, setDate] = useState(new Date());
 
   function removeItem(data) {
-    const result = todo.filter((item) => item.title !== data.title);
+    const result = todo.filter((item) => item.id !== data.id);
     setTodo(result);
   }
 
   function onSubmit() {
-    setTodo([...todo, { title: title }]);
+    setTodo([...todo, { title: title, date: date, id: uuidv4() }]);
     setTitle("");
+    setDate(new Date());
   }
 
   return (
@@ -32,13 +41,10 @@ function List() {
           value={title}
           onChange={(event) => setTitle(event.target.value)}
         />
+        <DatePicker selected={date} onChange={setDate} />
         <input type="button" value="Add" onClick={onSubmit} />
         {todo.map((data) => (
-          <Item
-            key={JSON.stringify(data)}
-            itemData={data}
-            removeItem={removeItem}
-          />
+          <Item key={data.id} itemData={data} removeItem={removeItem} />
         ))}
       </div>
     </div>
