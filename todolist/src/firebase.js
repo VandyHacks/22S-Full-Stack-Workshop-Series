2524374539cc67e85fc7312cc7880d1d5a270965
Kevin Jin <kevjin2002@gmail.com>
2022-04-18 00:17:38 -0500

    Reset firebase config

--- todolist/src/firebase.js
+++ todolist/src/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXdkHMw8w3BqKmoAn94Kf93YtDT3Y09v8",
-  authDomain: "todo-list-dd643.firebaseapp.com",
-  projectId: "todo-list-dd643",
-  storageBucket: "todo-list-dd643.appspot.com",
-  messagingSenderId: "1037412888449",
-  appId: "1:1037412888449:web:f661b0000de21f53284280",
-  measurementId: "G-KDWL7WV84D",
+  apiKey: "AIzaSyCKy5xsnmp2gFIplEsLcd-a8rKwQ3dSkdE",
+  authDomain: "todolist-2af6d.firebaseapp.com",
+  databaseURL: "https://todolist-2af6d-default-rtdb.firebaseio.com",
+  projectId: "todolist-2af6d",
+  storageBucket: "todolist-2af6d.appspot.com",
+  messagingSenderId: "217635445139",
+  appId: "1:217635445139:web:f77e4680dd25ef5f26b066",
+  measurementId: "G-T4C71HDVVS",
 };
 
 const app = initializeApp(firebaseConfig);
